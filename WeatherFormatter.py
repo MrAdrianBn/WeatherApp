6e6f79d9393dbe348cbd93efe09332d1a02e973f
Adrian <adrian.kuba.bien@gmail.com>
2024-07-30 12:50:44 +0200

    weather information  successfully extracted

--- WeatherFormatter.py
+++ WeatherFormatter.py
@@ -1,3 +1,5 @@
+import json
+
 from Query import Query
 
 
@@ -20,14 +22,26 @@ class WeatherFormatter:
         query = Query(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}'
                       f'&appid=c614ed968bb3ba231528a9e4546a27f1&units={self.unit}')
 
-        info = query.send_query()
+        info = query.send_query()  # dict
+        temp = str(info).replace("'", '"')
 
-        counter = 0
+        info = json.loads(temp.replace('[', '').replace(']', ''))
+
         wanted_keys = ['description', 'icon', 'temp', 'feels_like', 'temp_min', 'temp_max', 'pressure', 'humidity',
                        'sea_level', 'grnd_level', 'visibility', 'speed', 'deg', 'all', 'sunrise', 'sunset', 'timezone']
-        for key in info:
-            if key in wanted_keys:
-                self.weather_information[counter] = info[f'{key}']
-                counter += 1
+
+        counter = 0
+        for x, obj in info.items():
+            if not isinstance(obj, int):
+                for y in obj:
+                    if y not in wanted_keys:
+                        continue
+                    else:
+                        self.weather_information[counter] = info[f'{x}'][f'{y}']
+                        counter += 1
+            else:
+                if x in wanted_keys:
+                    self.weather_information[counter] = info[f'{x}']
+                    counter += 1
 
         return self.get_weather_information()
